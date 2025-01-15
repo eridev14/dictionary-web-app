542f72d3c2c366fa9b3b23d5c7c65c330f2af490
erihc <eridev14hc@gmail.com>
2025-01-15 13:55:46 -0400

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.io/eridev14/dictionary-web-app/',
+  base: '/dictionary-web-app/',
 })
